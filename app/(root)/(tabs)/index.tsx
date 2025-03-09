de52f2da790c0344231128bd9f8b717790de734c
MauriNestor <mauric845apaza@gmail.com>
2025-03-08 22:21:36 -0400

    add features, search and see all

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,18 +1,36 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
+import Search from "@/components/Search";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold my-10 font-rubik-semibold text-3xl">
-        Wecome to Restate
-      </Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center">
+          <Image source={images.avatar} className="size-12 rounded-full" />
+          <View className="flex-1 flex-col items-start ml-2 justify-center">
+            <Text className="text-xs font-rubik text-black-100">
+              Good morning
+            </Text>
+            <Text className="text-base font-rubik-medium text-black-300">
+              Mauricio
+            </Text>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+        <Search />
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold">Featured</Text>
+            <Text className="text-base font-rubik-semibold text-primary-300">
+              See all
+            </Text>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
