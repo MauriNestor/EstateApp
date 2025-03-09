e7a92bae5bcf6b8f7af1e19632cb817f97cadb9c
MauriNestor <mauric845apaza@gmail.com>
2025-03-09 18:20:58 -0400

    add cads to home

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
 import Search from "@/components/Search";
+import { Card, FeatureCard } from "@/components/Cards";
 
 export default function Index() {
   return (
@@ -25,10 +26,29 @@ export default function Index() {
         <View className="my-5">
           <View className="flex flex-row items-center justify-between">
             <Text className="text-xl font-rubik-bold">Featured</Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-semibold text-primary-300">
+                See all
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View className="flex flex-row gap-5 mt-5">
+          <FeatureCard />
+          <FeatureCard />
+        </View>
+
+        <View className="flex flex-row items-center justify-between mt-2">
+          <Text className="text-xl font-rubik-bold">Our recomendation</Text>
+          <TouchableOpacity>
             <Text className="text-base font-rubik-semibold text-primary-300">
               See all
             </Text>
-          </View>
+          </TouchableOpacity>
+        </View>
+        <View className="flex flex-row gap-5 mt-5">
+          <Card />
+          <Card />
         </View>
       </View>
     </SafeAreaView>
